aab534ba493cc7bb73967d8ff032d8b5288612a4
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2020-11-07 00:54:06 -0600

    Importacion de Reac-Navigation

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 /// En este se importan los modulos necesarios para que la aplicacion funcione
 import React from "react";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator }  from "@react-navigation/stack";
+
+
 import OasisHomeSearch from "./src/screens/OasisHomeSearch"; // Ruta relativa dde donde se encuentra 
+
+
+import OasisSearchResults from "./src/screens/OasisSearchResults";
 
 
 export default function App() {
   /// Vamos a retorna dicha pantalla  como un componente <>
   return <OasisHomeSearch></OasisHomeSearch>
+
+    // return <OasisSearchResults></OasisSearchResults>
 
 }
