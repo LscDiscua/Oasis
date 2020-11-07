992bd4564449fd3e1e635a836b8a462e908a6ef1
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2020-11-07 00:51:52 -0600

    Se crearon las conexiones Con la api

---
+++ src/api/backend.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+import getEnvVars from "../../enviroment"
+
+
+const { apiUrl } = getEnvVars();
+
+
+const instance = axios.create({
+
+    baseURL: apiUrl
+});
+
+export default instance;
