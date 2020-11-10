c8aff7e62b429b55f3e21699244502ea784d9a91
Linda Sarahi Carranza Discua <61211637+LscDiscua@users.noreply.github.com>
2020-11-09 18:28:14 -0600

    Actualizacion de la api Key

--- src/api/backend.js
+++ src/api/backend.js
@@ -3,12 +3,19 @@ import axios from "axios";
 import getEnvVars from "../../enviroment"
 
 
-const { apiUrl } = getEnvVars();
+const { apiUrl, apiKey, apiHost, useQuery } = getEnvVars();
 
 
 const instance = axios.create({
 
-    baseURL: apiUrl
+    baseURL: apiUrl,
+    headers: { 
+        'x-rapidapi-key': apiKey, 
+        'x-rapidapi-host': apiHost, 
+        'useQueryString': useQuery
+
+      }
+
 });
 
 export default instance;
